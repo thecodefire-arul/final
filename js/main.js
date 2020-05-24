210a313f0e3a6e4050aa4d6a28ee8d68d9fb91a8
Aryan Sharma <aryansharma11223@gmail.com>
2020-05-24 13:19:23 +0530

    Navbar Changed

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 $(document).ready(function(){
     $("#menu").click(function(){
-        $("#navbar").fadeIn(500);
+        $("#navbar").fadeIn(200);
     })
 });
 
 //Close Navbar by clicking on both Button & Navbar Links
 
 $(document).ready(function(){
-    $(".nav-close, .nav-links").click(function(){
-        $("#navbar").fadeOut(500);
+    $("#close-nav, #navbar ul li").click(function(){
+        $("#navbar").fadeOut(200);
     })
 });
